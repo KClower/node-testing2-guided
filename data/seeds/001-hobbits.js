0e3bea7594e4a64e9b426598b30696520633b477
KClower <kclower3@gmail.com>
2024-08-28 14:49:38 -0500

    getting errors with production

--- data/seeds/001-hobbits.js
+++ data/seeds/001-hobbits.js
@@ -1,8 +1,9 @@
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries and resets ids
+  console.log(knex)
   return knex('hobbits')
     .truncate()
-    .then(function() {
+    .then(function () {
       return knex('hobbits').insert([
         { name: 'sam' },
         { name: 'frodo' },
